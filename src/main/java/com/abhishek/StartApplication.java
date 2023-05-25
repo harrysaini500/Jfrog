ed8a7d1eb86502d5f08f1ecc985a75659e6de9b5
Ubuntu <ubuntu@ip-10-0-7-252.ap-south-1.compute.internal>
2023-05-25 10:46:07 +0000

    hello world application

--- src/main/java/com/abhishek/StartApplication.java
+++ src/main/java/com/abhishek/StartApplication.java
@@ -2,21 +2,19 @@ package com.abhishek;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@Controller
+@RestController
 public class StartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+    }
 
     @GetMapping("/")
     public String helloWorld() {
         return "Hello World";
-    }
-    
-    public static void main(String[] args) {
-        SpringApplication.run(StartApplication.class, args);
     }
 
 }
